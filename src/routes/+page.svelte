6ff9bff6d84f1babee1de83a0eb54af6f2d36abc
Tee Ming <chewteeming01@gmail.com>
2024-03-23 23:02:06 +0800

    add project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,25 @@
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<h1>
+		<span class="welcome">
+			<enhanced:img src="$lib/images/svelte-welcome.png" alt="Welcome" />
+		</span>
+
+		to your new<br />SvelteKit app
+	</h1>
+</section>
+
+<style>
+	.welcome img {
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		top: 0;
+		display: block;
+	}
+</style>
